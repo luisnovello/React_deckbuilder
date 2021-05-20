0ef5150bb0c1ca1e9cfb25864281f6dd48f94792
novello99 <novellosal@gmail.com>
2021-05-20 17:57:16 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import axios from "axios";
+
+import { DeckList, SearchBar, SearchResults } from "./components";
+
+const App = () => {
+  const [results, setResults] = useState([]);
+
+  return (
+    <div id="app">
+      <SearchBar setResults={setResults} />
+      <SearchResults results={results} />
+      <DeckList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
